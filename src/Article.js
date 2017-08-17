4936734adaf86da712aa4f536a47af622da7a6b3
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-08-17 22:39:52 +0300

    add state

--- src/Article.js
+++ src/Article.js
@@ -1,5 +1,39 @@
-import React from 'react'
+import React, {Component} from 'react'
 
+class Article extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isOpen: true
+        }
+    }
+
+    render() {
+        const {article} = this.props
+
+        return (
+            <div>
+                <h3 onClick = {this.handleClick}>{article.title}</h3>
+                {this.getBody()}
+            </div>
+        )
+    }
+
+    handleClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    getBody() {
+        return this.state.isOpen && <p>{this.props.article.text}</p>
+    }
+}
+
+export default Article
+
+/*
 export default function Article(props) {
     const {article} = props
     return (
@@ -8,4 +42,4 @@ export default function Article(props) {
             <p>{article.text}</p>
         </div>
     )
-}
+}*/
