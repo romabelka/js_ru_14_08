c7068f2b38030082aaf3294f189c43e65c0fbd1f
Andrey <andrey.shlygin@goods.ru>
2017-08-19 01:16:47 +0300

    update comment component - add default values

--- src/Comment.js
+++ src/Comment.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Comment(props) {
 
-    const {user, text} = props;
+    const {user='Anonymous', text='Empty message'} = props;
 
     return <div>
         <h3>{user}</h3>
