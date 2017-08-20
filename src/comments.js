9322d7d615fbd5660cabad52146bf6b405275fce
egza <tech@tezart.ru>
2017-08-20 15:27:56 +0300

    first lesson homework

---
+++ src/comments.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import Comment from './Comment'
+
+class CommentsList extends Component {
+    constructor(props) {
+        super(props);
+
+        if (props.comments !== undefined) 
+            this.commentElements = props.comments.map(
+                comment => <li key={comment.id}><Comment comment={comment}/></li>
+            );
+
+        this.state = {
+            isOpenCommentsBlock: true
+        }
+    }
+    
+    render() {
+        //if we had no comments nothing to render
+        if (this.props.comments === undefined) return (null);
+        //else
+        return (
+            <ul>
+                <button onClick={this.toggleComments}>{this.getButtonName()}</button>
+                {this.getBody()}
+            </ul>
+        )
+    }
+
+    toggleComments = () => {
+        this.setState({
+            isOpenCommentsBlock: !this.state.isOpenCommentsBlock
+        });
+    }
+
+    getBody() {
+        return this.state.isOpenCommentsBlock && this.commentElements;
+    }
+
+    getButtonName() {
+        return this.state.isOpenCommentsBlock ? 'Hide':'Show';
+    }
+}
+
+// CommentsList.prototype.ToggleComments = function() {
+//     this.state.isOpenCommentsBlock = !this.state.isOpenCommentsBlock;
+// }
+
+export default CommentsList
