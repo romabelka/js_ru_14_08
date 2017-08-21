d555b5b4043bf88924d2069b502a2daa61b809b8
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-08-21 23:00:58 +0300

    add reverse data flow

--- src/components/Article.js
+++ src/components/Article.js
@@ -14,6 +14,7 @@ class Article extends Component {
 
     render() {
         const {article, toggleOpen} = this.props
+        console.log('---', toggleOpen)
 
         return (
             <div>
@@ -33,4 +34,4 @@ class Article extends Component {
     }
 }
 
-export default toggleOpen(Article)
+export default Article
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Article from './Article'
 
-function ArticleList(props) {
-    const articleElements = props.articles.map(article => <li key={article.id}><Article article={article}/></li>)
-    return (
-        <ul>
-            {articleElements}
-        </ul>
-    )
+class ArticleList extends Component {
+    state = {
+        openArticleId: null
+    }
+
+    render() {
+        const articleElements = this.props.articles.map(article => (
+            <li key={article.id}>
+                <Article
+                    article={article}
+                    isOpen={article.id === this.state.openArticleId}
+                    toggleOpen={this.toggleOpenArticle.bind(this, article.id)}
+                />
+            </li>
+        ))
+
+        return (
+            <ul>
+                {articleElements}
+            </ul>
+        )
+    }
+
+    toggleOpenArticle(openArticleId) {
+        this.setState({ openArticleId })
+    }
+
+/*
+    toggleOpenArticle = (openArticleId) => () => {
+        this.setState({ openArticleId })
+    }
+*/
 }
 
 export default ArticleList
