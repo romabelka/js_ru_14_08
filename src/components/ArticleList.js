fab78dd369f0c6c14d341d4860a91357da163328
Andrey <andrey.shlygin@goods.ru>
2017-08-23 17:58:21 +0300

    update articleList and commentList prop types

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react'
 import Article from './Article'
 import accordion from './../decorators/accordion.js'
+import PropTypes from 'prop-types'
 
 class ArticleList extends Component {
+
+    static propTypes = {
+
+        articles: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.string,
+                title: PropTypes.string.isRequired,
+                text: PropTypes.string
+            })
+        ),
+
+        toggleOpenArticle: PropTypes.func.isRequired
+    };
 
     render() {
         const articleElements = this.props.articles.map(article => {
