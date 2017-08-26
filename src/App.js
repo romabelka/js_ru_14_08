5e6fa14278ce06a04d3504fa778884389c301694
Yuri Shilka <yuri.shilka@gmail.com>
2017-08-26 14:02:18 +0300

    format and styling fun

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from 'react'
-import ArticleList from './ArticleList'
-
 export default function App(props) {
-    return (
-        <div style={{ width: '80%', margin: '0 auto' }}>
-            <h2>Menu</h2>
-            <div>
-                <h1>News App</h1>
-                <ArticleList articles = {props.articles}/>
-            </div>
-        </div>
-    )
+  return (
+    <div style={{ width: '80%', margin: '0 auto' }}>
+      <h2>Menu</h2>
+      <div>
+        <h1>News App</h1>
+        <ArticleList articles = {props.articles}/>
+      </div>
+    </div>
+  )
 }
