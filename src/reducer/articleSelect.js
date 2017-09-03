8882df16c5ab11232e00d2e54cccb16585d7f4a6
Dmitry Lapynov <lapynow.d@gmail.com>
2017-09-03 22:56:14 +0300

    task_4 remove selected value id article deleted;

--- src/reducer/articleSelect.js
+++ src/reducer/articleSelect.js
@@ -1,9 +1,12 @@
-import {SELECT_ARTICLE} from '../constants';
+import {DELETE_ARTICLE, SELECT_ARTICLE} from '../constants';
 
 export default (state = [], action) => {
     switch (action.type) {
         case SELECT_ARTICLE:
             return action.payload;
+        case DELETE_ARTICLE: {
+            return state.filter(item => item.value !== action.payload.id);
+        }
         default:
             return state;
     }
