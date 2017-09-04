4b3a8d9cf7510fd1ccf2538d65022388fc9f9343
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-09-04 22:47:31 +0300

    add logger middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
-import {createStore, compose} from 'redux'
+import {createStore, compose, applyMiddleware} from 'redux'
 import reducer from '../reducer'
+import logger from '../middlewares/logger'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
         // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
     }) : compose
 
-const enhancer = composeEnhancers()
+const enhancer = composeEnhancers(applyMiddleware(logger))
 
 const store = createStore(reducer, enhancer)
 window.store = store
