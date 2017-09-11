021526e54791f09df37ade6d2fb4e0eb6a04da00
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-09-11 22:55:33 +0300

    add some apis

--- src/components/Root.js
+++ src/components/Root.js
@@ -3,7 +3,7 @@ import ArticlesPage from './Routes/ArticlesPage'
 import UserForm from './UserForm'
 import Filters from './Filters'
 import Counter from './Counter'
-import {Route, Link} from 'react-router-dom'
+import {Route, Link, Switch} from 'react-router-dom'
 
 export default class Root extends Component {
     render() {
@@ -18,12 +18,17 @@ export default class Root extends Component {
                 <div>
                     <h1>News App</h1>
                     <UserForm />
-                    <Route path="/counter" component={Counter} />
-                    <Route path="/filters" component={Filters} />
-                    <Route path="/articles" component={ArticlesPage} />
+                    <Switch>
+                        <Route path="/counter" component={Counter} exact />
+                        <Route path="/filters" component={Filters} />
+                        <Route path="/articles/new" render={this.getArticleForm} />
+                        <Route path="/articles" component={ArticlesPage} />
+                    </Switch>
                 </div>
             </div>
         )
 
     }
+
+    getArticleForm = () => <h2>New Article form</h2>
 }
